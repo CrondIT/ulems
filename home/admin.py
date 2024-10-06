16818578269d4c85bca19ddf894e03ecca267492
CrondIT <vdknsm@gmail.com>
2024-10-06 16:50:39 +0300

    models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Person, Event, Category, Participant
 
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Event)
+admin.site.register(Category)
+admin.site.register(Participant)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+# class for record's created time and updated time
+class TimeStamp(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Event(TimeStamp):
+    title = models.CharField('Название', max_length=200, default='Новое мероприятие')
+    print_title = models.TextField('Название для печати')
+    description = models.TextField('Описание') 
+    from_date = models.DateField('Дата начала')
+    to_date = models.DateField('Дата окончания')
+   
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Мероприятие'
+        verbose_name_plural = 'Мероприятия'
+
+class Person(TimeStamp):
+    first_name = models.CharField("Имя", max_length=100)
+    middle_name = models.CharField("Фамилия", max_length=100)
+    last_name = models.CharField("Отчество", max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Физическое лицо'
+        verbose_name_plural = 'Физические лица'
+
+class Category(TimeStamp):
+    category = models.CharField("Категория участника", max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Категория участника'
+        verbose_name_plural = 'Категории участников'
+
+class Participant(TimeStamp):
+    name = models.ForeignKey(Person, on_delete=models.CASCADE, related_name="pname" )        
