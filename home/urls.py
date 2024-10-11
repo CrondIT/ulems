cba41431367ff8537178be1b088c5fccffeab2a0
crondit <vdknsm@gmail.com>
2024-10-11 11:14:46 +0300

    5

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ app_name = "home"
 urlpatterns=[
     path("", views.index, name="index"),
     path("events", views.events, name="events"),
-    path("<int:event_id>", views.event, name="event"),
-    path("categories", views.categories, name="categories")
+    path("event/<int:event_id>", views.event, name="event"),
+    path("categories", views.categories, name="categories"),
+    path("category/<int:category_id>", views.category, name="category"),
 ]
--- home/views.py
+++ home/views.py
@@ -12,7 +12,7 @@ def  events(request):
 
 def event(request, event_id):
     event = Event.objects.get(pk=event_id)
-    return render(request, "home/event.html", {
+    return render(request, "home/event/event.html", {
         "event": event
     })
      
@@ -20,4 +20,10 @@ def event(request, event_id):
 def  categories(request):
     return render(request, "home/categories.html", {
         "categories": Category.objects.all()
+    })
+
+def category(request, category_id):
+    category = Category.objects.get(pk=category_id)
+    return render(request, "home/category/category.html", {
+        "category": category
     })
