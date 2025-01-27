fd55aa36c50033a0bfee7ee3244243fddd744a44
crondit <vdknsm@gmail.com>
2025-01-27 14:03:26 +0300

    n

---
+++ home/import_export.py
@@ -0,0 +1,10 @@
+import csv
+
+def export(filename, data):
+    """ export data to csv file
+
+    Args:
+        filename (string): path and name of the csv file
+        data (dictionary): data to export
+    """
+    pass
