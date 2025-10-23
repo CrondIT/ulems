a5213ad4c335f3cda8d7d9ba245245a73ef099f0
CrondIT <vdknsm@gmail.com>
2025-10-23 07:25:19 +0300

    tests

--- ulems/settings.py
+++ ulems/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users',
-    "home",
+    'home',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
+# app_name = "users"
 urlpatterns = [
     path("", views.index, name="index"),
     # path("login", views.login_view, name="login"),
